783a332b3f4314765f19c7f8947589e20ec0eb71
Overlord <tanvir1005@student.sust.edu>
2021-12-02 23:10:26 +0600

    Initial commit

---
+++ app/src/main/java/com/ahmed/memoriar/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ahmed.memoriar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var clRoot: CoordinatorLayout
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clRoot = findViewById(R.id.clRoot)
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this,2)
+    }
+}
---
+++ app/src/main/java/com/ahmed/memoriar/MemoryBoardAdapter.kt
@@ -0,0 +1,20 @@
+package com.ahmed.memoriar
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import java.security.AccessControlContext
+
+class MemoryBoardAdapter(private val context: Context,private val numPieces: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount() = numPieces
+
+}
