cc926a0fa5efd7c8063dad8fc70517c4a152aa71
Overlord <tanvir1005@student.sust.edu>
2021-12-03 23:52:42 +0600

    4nd commit

--- app/src/main/java/com/ahmed/memoriar/MainActivity.kt
+++ app/src/main/java/com/ahmed/memoriar/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ahmed.memoriar.models.BoardSize
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
+
+    private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +26,9 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize)
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this,2)
+        rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth())
 
     }
 }
